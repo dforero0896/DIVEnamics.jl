783b4477a0ba5123c8c189837b8f51f9f23c598e
Daniel Forero-Sanchez <df.forero10@gmail.com>
2022-09-10 15:56:40 +0200

    Adding DTFE

--- src/DIVEnamics.jl
+++ src/DIVEnamics.jl
@@ -5,6 +5,7 @@ using StaticArrays
 using Zygote
 
 export delaunay, tetrahedra_to_voids
+export dtfe
 
 GeometricalPredicates.Point(vec) = Point3D(Float64(vec[1]), Float64(vec[2]), Float64(vec[3]))
 GeometricalPredicates.Primitive(matrix::SMatrix{3,4}) = Primitive([Point(matrix[:,j]) for j in 1:4]...)
@@ -13,11 +14,8 @@ function delaunay(points::Matrix)
     println("Starting Delaunay Triangulation with ", size(points, 2), " points.")
     @time begin
         input=TetGen.RawTetGenIO{Cdouble}(pointlist=points)
-        tmp = tetrahedralize(input, "w")
-        println(tmp.neighborlist)
-        println(tmp.facetlist)
-        println(input.neighborlist)
-        return tmp.tetrahedronlist
+        tmp = tetrahedralize(input, "n")
+        return tmp.tetrahedronlist, tmp.neighborlist
     end
 end
 
@@ -40,12 +38,13 @@ function tetrahedra_to_voids(points::Matrix{Float64}, tetrahedra_indices::Matrix
     final_cat = zeros(5, size(tetrahedra_indices,2))
     if derivatives
         println("Allocating data for derivatives.")
-        dx_dposs = Array{Float64, 3}(undef, (3, 4, size(tetrahedra_indices,2)))
-        dy_dposs = Array{Float64, 3}(undef, (3, 4, size(tetrahedra_indices,2)))
-        dz_dposs = Array{Float64, 3}(undef, (3, 4, size(tetrahedra_indices,2)))
-        dvol_dposs = Array{Float64, 3}(undef, (3, 4, size(tetrahedra_indices,2)))
-        dr_dposs = Array{Float64, 3}(undef, (3, 4, size(tetrahedra_indices,2)))
+        dx_dpos = Array{Float64, 3}(undef, (3, 4, size(tetrahedra_indices,2)))
+        dy_dpos = Array{Float64, 3}(undef, (3, 4, size(tetrahedra_indices,2)))
+        dz_dpos = Array{Float64, 3}(undef, (3, 4, size(tetrahedra_indices,2)))
+        dvol_dpos = Array{Float64, 3}(undef, (3, 4, size(tetrahedra_indices,2)))
+        dr_dpos = Array{Float64, 3}(undef, (3, 4, size(tetrahedra_indices,2)))
     end
+
     for i in 1:size(tetrahedra_indices,2)
         
         vertex_coordinates = SMatrix{3,4}(points[:,tetrahedra_indices[:,i]])
@@ -59,21 +58,47 @@ function tetrahedra_to_voids(points::Matrix{Float64}, tetrahedra_indices::Matrix
         final_cat[4,i] = radius        
         final_cat[5,i] = vol
         if derivatives
-            view(dx_dposs, :,:,i) .= gradient(circumcenter_x, vertex_coordinates)[1]
-            view(dy_dposs, :,:,i) .= gradient(circumcenter_y, vertex_coordinates)[1]
-            view(dz_dposs, :,:,i) .= gradient(circumcenter_z, vertex_coordinates)[1]
-            view(dvol_dposs, :,:,i) .= gradient(GeometricalPredicates.volume, vertex_coordinates)[1]
-            view(dr_dposs, :,:,i) .= gradient(circumradius, vertex_coordinates)[1]
+            view(dx_dpos, :,:,i) .= gradient(circumcenter_x, vertex_coordinates)[1]
+            view(dy_dpos, :,:,i) .= gradient(circumcenter_y, vertex_coordinates)[1]
+            view(dz_dpos, :,:,i) .= gradient(circumcenter_z, vertex_coordinates)[1]
+            view(dvol_dpos, :,:,i) .= gradient(GeometricalPredicates.volume, vertex_coordinates)[1]
+            view(dr_dpos, :,:,i) .= gradient(circumradius, vertex_coordinates)[1]
         end
         
         
     end
+
+    if derivatives
+        return final_cat, dx_dpos, dy_dpos, dz_dpos, dvol_dpos, dr_dpos
+    else
+        return final_cat
+    end
+
     
-    final_cat
+end
+
+function dtfe(points::Matrix{Float64}, tetrahedra_indices::Matrix{Int32}, neighbor_list::Matrix{Int32})
+    
+    dtfe_arr = zeros(size(points,2))
+    for i in 1:size(tetrahedra_indices,2)
+
+        vertex_coordinates = SMatrix{3,4}(points[:,tetrahedra_indices[:,i]])
+        vol = GeometricalPredicates.volume(vertex_coordinates)
+        
+        for j in 1:4
+            dtfe_arr[tetrahedra_indices[j,i]] += vol
+        end
 
 
+    end
+
+    return 4 ./ dtfe_arr
 
 end
 
 
+
+
+#Module end
 end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,18 @@
 using DIVEnamics
 using Test
+using Random
 
 @testset "DIVEnamics.jl" begin
     # Write your tests here.
 end
 
-points = 1000 * rand(3, 1000)
-simplex_indices = delaunay(points)
-@time void_cat = tetrahedra_to_voids(points, simplex_indices, true)
+points = 1000 * rand(MersenneTwister(42), 3, 1000)
+point_attrs = randn(MersenneTwister(0), 1, 1000)
+simplex_indices, simplex_nbs = delaunay(points)
+@time void_cat = tetrahedra_to_voids(points, simplex_indices, false)
+local_density = dtfe(points, simplex_indices, simplex_nbs)
 println(void_cat[:,1])
+
 #points = 1000 * rand(3, 1000000)
 #simplex_indices = delaunay(points)
 #@time void_cat = tetrahedra_to_voids(points, simplex_indices, false)
